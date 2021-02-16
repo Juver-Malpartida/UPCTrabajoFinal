6665065a87148dafcf7758059dbe357d33a3aa94
Juver Malpartida <juver.malpartida@bw2technologies.com>
2021-02-16 02:33:36 -0500

    update

--- src/Modelos/Curso.java
+++ src/Modelos/Curso.java
@@ -1,13 +1,18 @@
 package Modelos;
 
+import java.util.ArrayList;
+
 public class Curso {
     private int ID;
     private String nombre;
     private Profesor profesor;
+    private ArrayList<Alumno> alumnos;
 
     public Curso(int ID, String nombre, Profesor profesor) {
         this.ID = ID;
         this.nombre = nombre;
+        this.profesor = profesor;
+        alumnos = new ArrayList<>();
     }
 
     public int getID() {
@@ -22,4 +27,38 @@ public class Curso {
         return profesor;
     }
 
+    public void saveAlumno(Alumno alumno) {
+        if (alumnos.contains(alumno)) {
+            return;
+        }
+        alumnos.add(alumno);
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Curso: " + getNombre());
+        System.out.println("Profesor: " + profesor.getNombre());
+        System.out.println("Alumnos: ");
+
+        double[] promediosArr = new double[alumnos.size()];
+        for (int i = 0; i <= alumnos.size() - 1; i++) {
+            Alumno alumno = alumnos.get(i);
+            double promedioAlumno = alumno.getPromedioPorCurso(this);
+            promediosArr[i] = promedioAlumno;
+            System.out.println(alumno.getID() + " " + alumno.getNombre() + " " + alumno.getPromedioPorCurso(this));
+        }
+
+        System.out.println("Promedio del Curso: " + getPromedio(promediosArr));
+
+        System.out.println("");
+    }
+
+    private double getPromedio(double[] arrNumbers) {
+        double sum = 0;
+
+        for (int a = 0; a <= arrNumbers.length - 1; a++) {
+            sum = sum + arrNumbers[a];
+        }
+
+        return sum / arrNumbers.length;
+    }
 }
